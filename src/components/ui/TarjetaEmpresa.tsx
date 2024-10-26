d4947a5b70ab5ade9b7074fea08a2a560ce2f789
FrancoUtn <francosardiutn@gmail.com>
2024-10-26 14:02:33 -0300

    Creando Home base con modal de empresa + agregar empresa

---
+++ src/components/ui/TarjetaEmpresa.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react"
+import { IEmpresa } from "../types/IEmpresa"
+
+
+export const TarjetaEmpresa: FC<IEmpresa>  = () => {
+  return (
+    <div>
+        <h2>{}</h2>
+    </div>
+  )
+}
